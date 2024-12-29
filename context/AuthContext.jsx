b3210076fec571918dc54e49bd279dc856b74dd9
Abdullah Zafar <abdullahzafarrana@gmail.com>
2024-12-29 12:45:04 +0500

    added AuthContext

---
+++ context/AuthContext.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+
+const API_URL = "http://192.168.137.252:6969/api";
+const TOKEN_KEY = "jwt";
+const AuthContext = createContext({});
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [authState, setAuthState] = useState({
+    token: null,
+    authenticated: false,
+  });
+
+  useEffect(() => {
+    const loadToken = async () => {
+      const token = await SecureStore.getItemAsync(TOKEN_KEY);
+
+      if (token) {
+        setAuthState({ token, authenticated: true });
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      }
+    };
+    loadToken();
+  }, []);
+
+  const register = async (email, password) => {
+    try {
+      console.log("register");
+      const response = await axios.post(`${API_URL}/signup`, {
+        email,
+        password,
+      });
+      console.log("Response:");
+      console.log(response.data);
+      return response.data;
+    } catch (e) {
+      console.log(e);
+      return { error: true, msg: e.message };
+    }
+  };
+
+  const login = async (email, password) => {
+    //prettier-ignore
+    try {
+      console.log("login")
+      const response = await axios.post(`${API_URL}/signin`, { email, password });
+      setAuthState({ token: response.data.token, authenticated: true });
+      axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.token}`;
+      SecureStore.setItemAsync(TOKEN_KEY, response.data.token);
+      return response.data;
+    } catch (e) {
+      return { error: true, msg: e.message };
+    }
+  };
+
+  const logout = async () => {
+    await SecureStore.deleteItemAsync(TOKEN_KEY);
+    axios.defaults.headers.common["Authorization"] = "";
+    setAuthState({ token: null, authenticated: false });
+  };
+
+  const value = {
+    onRegister: register,
+    onLogin: login,
+    onLogout: logout,
+    authState,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
