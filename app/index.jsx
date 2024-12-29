064bf57743f1aabeaa57980c44652b6edf452ca4
ApexArbiter <mahadwebdev@gmail.com>
2024-12-29 14:13:12 +0500

    events

--- app/index.jsx
+++ app/index.jsx
@@ -15,6 +15,7 @@ export default function Index() {
         <Link className="bg-gray-200" href={"(auth)/register"}>register</Link>
         <Link className="bg-gray-200" href={"(auth)/login"}>login</Link>
         <Link className="bg-gray-200" href={"/frontend"}>Frontend</Link>
+        <Link className="bg-gray-200" href={"/events/new"}>add Event</Link>
       </View>
     </View >
   );
